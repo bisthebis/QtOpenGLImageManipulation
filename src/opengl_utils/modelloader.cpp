be251721910ccc0e4d9f7e87fd8270e77fd1c072
bisthebis <bism25@hotmail.com>
2017-01-03 21:05:11 +0100

    Model Loader

---
+++ src/opengl_utils/modelloader.cpp
@@ -0,0 +1,140 @@
+#include "modelloader.h"
+#include <QUrl>
+#include <QTextStream>
+#include <QStringList>
+#include <QRegularExpression>
+#include <QCoreApplication>
+
+
+//Utility functions
+static QStringList readFile(const QUrl& path);
+static const QRegularExpression space_split("\\s+");
+
+ModelLoader::ModelLoader()
+{
+
+}
+
+void ModelLoader::loadFile(const QUrl &path)
+{
+    QStringList lines = readFile(path);
+
+
+
+    for (QString line : lines)
+    {
+        if (line.startsWith("v") && !line.startsWith("vt") && !line.startsWith("vn"))
+        {
+            //It is a vertex position description
+            QStringList words = line.split(space_split);
+            bool OK = true;
+            //words[0] is "v"
+            float x = words[1].toFloat(&OK);
+            float y = words[2].toFloat(&OK);
+            float z = words[3].toFloat(&OK);
+            if (!OK)
+                qApp->exit(-1);
+
+            vertices.append(QVector3D(x, y, z));
+        }
+        else if (line.startsWith("vn"))
+        {
+            //It is a normal position description
+            QStringList words = line.split(space_split);
+            bool OK = true;
+            //words[0] is "v"
+            float x = words[1].toFloat(&OK);
+            float y = words[2].toFloat(&OK);
+            float z = words[3].toFloat(&OK);
+            if (!OK)
+                qApp->exit(-1);
+
+            normals.append(QVector3D(x, y, z));
+        }
+        else if (line.startsWith("vt"))
+        {
+            //It is a textures coordinates description
+            QStringList words = line.split(space_split);
+            bool OK = true;
+            //words[0] is "v"
+            float x = words[1].toFloat(&OK);
+            float y = words[2].toFloat(&OK);
+            if (!OK)
+                qApp->exit(-1);
+
+            texturesCoordinates.append(QVector2D(x, y));
+        }
+        else if (line.startsWith("f"))
+        {
+            //It's a face
+            line.replace('/', ' ');
+            auto values = line.split(space_split);
+            //values[0] is "f"
+            bool OK = true;
+            int v1 = values[1].toInt(&OK);
+            int vt1 = values[2].toInt(&OK);
+            int vn1 = values[3].toInt(&OK);
+
+            int v2 = values[4].toInt(&OK);
+            int vt2 = values[5].toInt(&OK);
+            int vn2 = values[6].toInt(&OK);
+
+            int v3 = values[7].toInt(&OK);
+            int vt3 = values[8].toInt(&OK);
+            int vn3 = values[9].toInt(&OK);
+
+            if (!OK)
+                qApp->exit(-1);
+
+            triangles.append({
+                                 {vertices[v1], texturesCoordinates[vt1], normals[vn1]},
+                                 {vertices[v2], texturesCoordinates[vt2], normals[vn2]},
+                                 {vertices[v3], texturesCoordinates[vt3], normals[vn3]}
+                             });
+
+
+        }
+    }
+
+
+
+}
+
+QVector<float> ModelLoader::toVBO(ModelLoader::ExportCategory type)
+{
+    QVector<float> result;
+    if (type == ExportCategory::VerticesThenUVs)
+    {
+        for (const Triangle& triangle : triangles)
+        {
+            for (const Vertex& vertex : triangle.vertices)
+            {
+                result.append(vertex.pos.x());
+                result.append(vertex.pos.y());
+                result.append(vertex.pos.z());
+                result.append(vertex.uvs.x());
+                result.append(vertex.uvs.y());
+            }
+        }
+    }
+
+
+
+    return result;
+}
+
+static QStringList readFile(const QUrl &path)
+{
+    QFile file(path.path());
+    QTextStream stream(&file);
+    QStringList lines;
+    while (true) {
+        QString line = stream.readLine();
+        if (line.isEmpty())
+            break;
+        lines << line;
+    }
+
+    file.close();
+    return lines;
+}
---
+++ src/opengl_utils/modelloader.h
@@ -0,0 +1,41 @@
+#ifndef MODELLOADER_H
+#define MODELLOADER_H
+
+#include <QFile>
+#include <QVector>
+#include <QVector3D>
+#include <QVector2D>
+
+
+
+
+class ModelLoader
+{
+public:
+    ModelLoader();
+    void loadFile(const QUrl& path);
+
+    enum ExportCategory {VerticesThenUVs};
+    QVector<float> toVBO(ExportCategory type);
+
+    struct Vertex {
+        QVector3D pos;
+        QVector2D uvs;
+        QVector3D normals;
+    };
+
+    struct Triangle {
+        Vertex vertices[3];
+    };
+
+
+
+private:
+    // "Padding" because in file, indexes start with 1 instead of 0.
+    QVector<QVector3D> vertices = {QVector3D(0,0,0)};
+    QVector<QVector3D> normals = {QVector3D(0,0,0)};
+    QVector<QVector2D> texturesCoordinates = {QVector2D(0,0)};
+    QVector<Triangle> triangles;
+};
+
+#endif // MODELLOADER_H
