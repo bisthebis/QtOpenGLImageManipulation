bfc747649e60fa3a8d5e05da1f86afc38147401d
bisthebis <bism25@hotmail.com>
2017-01-03 14:16:54 +0100

    Working colored cube

--- src/opengl_utils/cube.cpp
+++ src/opengl_utils/cube.cpp
@@ -1,6 +1,100 @@
 #include "cube.h"
 
+#include <QVector3D>
+#include <QOpenGLShaderProgram>
+
+struct VertexData
+{
+    QVector3D position;
+    QVector3D color;
+};
+
 Cube::Cube()
 {
+    initializeOpenGLFunctions();
+
+    vbo.create();
+    vao.create();
+
+    init();
+}
+
+Cube::~Cube()
+{
+    vbo.destroy();
+    vao.destroy();
+}
+
+void Cube::init()
+{
+    static const VertexData vertices[] =
+    {
+        //Superior face then inferior face, red
+        {QVector3D(-1, 1, 1), QVector3D(1, 0, 0)},
+        {QVector3D(-1, -1, 1), QVector3D(1, 0, 0)},
+        {QVector3D(1, 1, 1), QVector3D(1, 0, 0)},
+        {QVector3D(1, -1, 1), QVector3D(1, 0, 0)},
+        {QVector3D(-1, -1, 1), QVector3D(1, 0, 0)},
+        {QVector3D(1, 1, 1), QVector3D(1, 0, 0)},
+
+        {QVector3D(-1, 1, -1), QVector3D(1, 0, 0)},
+        {QVector3D(-1, -1, -1), QVector3D(1, 0, 0)},
+        {QVector3D(1, 1, -1), QVector3D(1, 0, 0)},
+        {QVector3D(1, -1, -1), QVector3D(1, 0, 0)},
+        {QVector3D(-1, -1, -1), QVector3D(1, 0, 0)},
+        {QVector3D(1, 1, -1), QVector3D(1, 0, 0)},
+
+        //Lateral faces, blue. Constant x
+        {QVector3D(-1, 1, 1), QVector3D(0, 0, 1)},
+        {QVector3D(-1, -1, 1), QVector3D(0, 0, 1)},
+        {QVector3D(-1, 1, -1), QVector3D(0, 0, 1)},
+        {QVector3D(-1, -1, -1), QVector3D(0, 0, 1)},
+        {QVector3D(-1, -1, 1), QVector3D(0, 0, 1)},
+        {QVector3D(-1, 1, -1), QVector3D(0, 0, 1)},
+
+        {QVector3D(1, 1, 1), QVector3D(0, 0, 1)},
+        {QVector3D(1, -1, 1), QVector3D(0, 0, 1)},
+        {QVector3D(1, 1, -1), QVector3D(0, 0, 1)},
+        {QVector3D(1, -1, -1), QVector3D(0, 0, 1)},
+        {QVector3D(1, -1, 1), QVector3D(0, 0, 1)},
+        {QVector3D(1, 1, -1),QVector3D(0, 0, 1)},
+
+        //Other lateral faces, constant y, gren
+        {QVector3D(1, 1, 1), QVector3D(0, 1, 0)},
+        {QVector3D(-1, 1, 1), QVector3D(0, 1, 0)},
+        {QVector3D(1, 1, -1), QVector3D(0, 1, 0)},
+        {QVector3D(-1, 1, -1), QVector3D(0, 1, 0)},
+        {QVector3D(-1, 1, 1), QVector3D(0, 1, 0)},
+        {QVector3D(1, 1, -1), QVector3D(0, 1, 0)},
+
+        {QVector3D(1, -1, 1), QVector3D(0, 1, 0)},
+        {QVector3D(-1, -1, 1), QVector3D(0, 1, 0)},
+        {QVector3D(1, -1, -1), QVector3D(0, 1, 0)},
+        {QVector3D(-1, -1, -1), QVector3D(0, 1, 0)},
+        {QVector3D(-1, -1, 1), QVector3D(0, 1, 0)},
+        {QVector3D(1, -1, -1),QVector3D(0, 1, 0)}
+
+
+    };
+
+    vao.bind();
+
+    vbo.bind();
+    vbo.allocate(vertices, sizeof(vertices));
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), 0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (char*)12);
+    glEnableVertexAttribArray(1);
+
+}
+
+void Cube::draw(QOpenGLShaderProgram& program)
+{
+    program.bind();
+    vao.bind();
+
+    glDrawArrays(GL_TRIANGLES, 0, 36);
 
 }
--- src/opengl_utils/cube.h
+++ src/opengl_utils/cube.h
@@ -2,11 +2,24 @@
 #define CUBE_H
 
 #include <QOpenGLFunctions>
+#include <QOpenGLBuffer>
+#include <QOpenGLVertexArrayObject>
+
+class QOpenGLShaderProgram;
 
 class Cube : public QOpenGLFunctions
 {
 public:
     Cube();
+    virtual ~Cube();
+
+    void draw(QOpenGLShaderProgram&);
+
+private:
+    void init();
+
+    QOpenGLBuffer vbo;
+    QOpenGLVertexArrayObject vao;
 };
 
 #endif // CUBE_H
